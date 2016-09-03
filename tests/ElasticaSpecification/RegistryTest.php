30910b9e8e45ccff6a7bc95cba83c62b97c4bf1e
GBProd <contact@gb-prod.fr>
2016-09-03 10:00:19 +0200

    feat: registry (#2)

---
+++ tests/ElasticaSpecification/RegistryTest.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Tests\GBProd\ElasticaSpecification;
+
+use GBProd\ElasticaSpecification\ExpressionBuilder\Builder;
+use GBProd\ElasticaSpecification\Registry;
+use GBProd\Specification\Specification;
+
+/**
+ * Tests for registry
+ *
+ * @author gbprod <contact@gb-prod.fr>
+ */
+class RegistryTest extends \PHPUnit_Framework_TestCase implements Specification
+{
+    public function testConstruct()
+    {
+        $registry = new Registry();
+
+        $this->assertInstanceOf(Registry::class, $registry);
+    }
+
+    public function isSatisfiedBy($candidate)
+    {
+        return true;
+    }
+
+    public function testGetBuilderThrowsOutOfRangeExceptionIfBuilderNotRegistred()
+    {
+        $registry = new Registry();
+
+        $this->setExpectedException(\OutOfRangeException::class);
+
+        $registry->getBuilder($this);
+    }
+
+    public function testGetBuilderReturnsAssociatedBuilder()
+    {
+        $registry = new Registry();
+
+        $builder = $this->prophesize(Builder::class)->reveal();
+
+        $registry->register(self::class, $builder);
+
+        $this->assertEquals(
+            $builder,
+            $registry->getBuilder($this)
+        );
+    }
+}
